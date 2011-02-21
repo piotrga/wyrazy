e80279f3a48b282d81e43a954fea277687944dcb
Piotr Gabryanczyk <piotrga@crystalbrains.com>
2011-02-21 21:22:49 +0000

    translator

--- src/main/scala/googletranslate/Translator.scala
+++ src/main/scala/googletranslate/Translator.scala
@@ -2,10 +2,10 @@ package googletranslate
 
 import java.net.URLEncoder
 import scala.io.Source
+import scala.util.parsing.json.JSON
 
-class Translator(from:String, to:String, key: String){
-  import scala.util.parsing.json.JSON
-  val url = "https://www.googleapis.com/language/translate/v2?key="+key+"&source="+from+"&target="+to
+class Translator(from:String, to:String, apiKey: String){
+  val baseUrl = "https://www.googleapis.com/language/translate/v2?key="+apiKey+"&source="+from+"&target="+to
 
   def translate(phrases:Seq[String]) = {
     val chunked = phrases.grouped(20).toList
@@ -13,10 +13,11 @@ class Translator(from:String, to:String, key: String){
   }
 
   def translateFew(phrases:Seq[String]) = {
-    val response = Source.fromURL(url + phrases.map("&q=" + URLEncoder.encode(_)).mkString).mkString
+    val url = baseUrl + phrases.map("&q=" + URLEncoder.encode(_, "UTF-8")).mkString
+    val response = Source.fromURL(url, "UTF-8").mkString
     JSON.parseFull(response) match{
-      case Some(map:Map[String, Map[String, List[Map[String,String]]]]) => map("data")("translations").map( _("translatedText"))
-      case x => throw new RuntimeException("Wrong answer "+x)
+      case Some(response : Map[String, Map[String, List[Map[String,String]]]]) => response("data")("translations").map( _("translatedText"))
+      case x => throw new RuntimeException("Unexpected response format: " + response)
     }
   }
 }
--- src/test/scala/googletranslate/TranslatorTest.scala
+++ src/test/scala/googletranslate/TranslatorTest.scala
@@ -1,27 +1,32 @@
 package googletranslate
 
-import googletranslate.Translator
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 import scala.collection.immutable._
 
 class TranslatorTest extends FlatSpec with ShouldMatchers{
-  object PL2EN extends Translator("pl", "en", "AIzaSyBA45xu5yZDal-p49_wKu8fLsmMAMQ6uuU")
+  object PL2EN extends Translator(from = "pl", to = "en", apiKey = "AIzaSyBA45xu5yZDal-p49_wKu8fLsmMAMQ6uuU")
+
+  implicit def int2ListBuilder(src : Int) = new ListBuilder(src)
+
 
   "EN2PL" should "translate one word" in{
     PL2EN.translate(List("slowo")) should be (List("word"))
   }
+
   it should "translate multiple words" in{
     PL2EN.translate(List("slowo", "ptak")) should be (List("word", "bird"))
   }
 
   it should "translate big batches of words" in{
-    PL2EN.translate(repeat("slowo",  100)) should be (repeat("word",  100))
+    PL2EN.translate(100 times "slowo") should be (100 times "word")
   }
 
-  def repeat(word: String, num: Int) = {
-    for (i<- 1 to num) yield word
+  class ListBuilder(repeat:Int){
+    def times[T](element : T) = for (i<- 1 to repeat) yield element
   }
+
+
 }
 
 
